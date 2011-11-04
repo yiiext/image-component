e081f0516ac79e3f6caeebbefa96c6fad31fd2ca
Alexander Kochetov <creocoder@yandex.ru>
2011-11-04 18:43:30 +0400

    add phpdoc

--- ImageComponent.php
+++ ImageComponent.php
@@ -9,7 +9,7 @@
 /**
  * Image processing with Imagine.
  *
- * @version 0.02
+ * @version 0.03
  * @package yiiext.image-component
  */
 class ImageComponent extends CApplicationComponent
@@ -20,15 +20,28 @@ class ImageComponent extends CApplicationComponent
 	public $driver='Gd';
 	private $_imagine;
 
+	/**
+	 * Initializes the application component.
+	 */
 	public function init()
 	{
 		Yii::setPathOfAlias('Imagine',__DIR__.'/Imagine');
 		$className="Imagine\\{$this->driver}\\Imagine";
 		$this->_imagine=new $className;
+		parent::init();
 	}
 
-	public function __call($name,$args)
+	/**
+	 * Calls the named method which is not a class method.
+	 * @param string $name the method name
+	 * @param array $parameters method parameters
+	 * @return mixed the method return value
+	 */
+	public function __call($name,$parameters)
 	{
-		return call_user_func_array(array($this->_imagine,$name),$args);
+		if(method_exists($this->_imagine,$name))
+			return call_user_func_array(array($this->_imagine,$name),$parameters);
+
+		return parent::__call($name,$parameters);
 	}
 }
